483975b8dfc6eda33070be8993602d375f2b9718
Geeky-Gyan <mishrarashmita200@gmail.com>
2024-06-05 21:44:41 +0530

    added files

---
+++ app.js
@@ -0,0 +1,85 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-storage.js";
+
+
+
+const firebaseConfig = ({
+    apiKey: "AIzaSyCdKthmN5V1tKXJ5vlMxbqc1L4o4clEKj4",
+  authDomain: "techteamtask2024.firebaseapp.com",
+  projectId: "techteamtask2024",
+  storageBucket: "techteamtask2024.appspot.com",
+  messagingSenderId: "1067539417498",
+  appId: "1:1067539417498:web:cb7f4bf39e824524ce1223",
+  measurementId: "G-76JGN8JWVE"
+  });
+
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+const collectionRef = collection(firestore, 'GyanajyotiPharma');
+const storage = getStorage(firebaseApp);
+
+
+function validateEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ ;
+    return emailRegex.test(email);
+}
+
+
+function validatePhoneNumber(phoneNumber) {
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phoneNumber);
+}
+
+
+
+    const form = document.getElementById('form');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        try {
+            
+            const name = getVal('name');
+            const branch = getVal('branch');
+            const emailid = getVal('emailid');
+            const Phonenumber = getVal('Phonenumber');
+            const imageFile = document.getElementById('fileInput').files[0];
+
+           
+            if (!validateEmail(emailid)) {
+                throw new Error('Invalid email address');
+            }
+
+            if (!validatePhoneNumber(Phonenumber)) {
+                throw new Error('Invalid phone number should be 10 digits');
+            }
+
+            const imageName = `${name}${branch}`;
+            const imageRef = ref(storage, imageName);
+            await uploadBytes(imageRef, imageFile);
+
+            const imageURL = await getDownloadURL(imageRef);
+
+            const formdata = {
+             name :   name,
+             branch :  branch, 
+             emailid  : emailid, 
+             Phonenumber : Phonenumber,
+             image : imageURL
+            };
+            const docRef = await addDoc(collectionRef, formdata);
+
+            form.reset();
+            alert('Form submitted successfully!!');
+        } catch (error) {
+            form.reset();
+            console.error('Form validation error:', error.message);
+            alert('Form validation error. Please check your inputs.');
+        }
+    });
+
+function getVal(id) {
+    return document.getElementById(id).value;
+}
